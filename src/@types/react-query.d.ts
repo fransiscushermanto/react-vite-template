0915c036cc223c6c436131d07125bc1cab3828be
Fransiscus Hermanto <fransiscus.huang47@gmail.com>
2025-07-17 01:19:48 +0700

    docs: enhance QueryMeta type definition

--- src/@types/react-query.d.ts
+++ src/@types/react-query.d.ts
@@ -4,7 +4,8 @@ import type { HttpOptions } from "@/utils/react-query";
 declare module "@tanstack/react-query" {
   interface QueryMeta
     extends DefaultQueryMeta,
-      Omit<HttpOptions, "params" | "body" | "path"> {
+      Omit<HttpOptions, "params" | "body" | "path">,
+      Record<string, unknown> {
     /**
      * To replace env basePath
      */
